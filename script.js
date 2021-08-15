e1069944fcc0a8291c69dee3eb9b6efdb330d18b
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-08-15 13:11:56 -0700

    initial HTML, CSS, Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+var rotatingPages = document.querySelector("#rotating-pages");
+var timer = document.querySelector("#timer");
+var page1 = document.querySelector("#page1");
+var page4 = document.querySelector("#page4");
+var startQuiz = document.querySelector("#start-quiz");
+
