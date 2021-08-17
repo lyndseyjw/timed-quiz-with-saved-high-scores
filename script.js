9b76de16ac0016e48dbde5f30c0bcf2f9a204eec
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-08-16 17:39:35 -0700

    correct answer button now moves onto the next question, styling of answer buttons, added set / get local storage commands

--- script.js
+++ script.js
@@ -6,16 +6,11 @@ var startQuiz = document.querySelector("#start-quiz");
 
 var secondsLeft = 60;
 var score = 0;
-
-function secondPage() {
-
-    startTimer();
-    question1();
-}
+var timerInterval;
 
 function startTimer() {
 
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
 
         secondsLeft--;
         timer.textContent = secondsLeft
@@ -30,63 +25,70 @@ function startTimer() {
     return secondsLeft;
 }
 
-function question1() {
+if (startQuiz){
 
-    page1.setAttribute("style", "visibility: hidden;");
+    startQuiz.addEventListener("click", function() {
 
-    var page2header = document.createElement("h2");
-    page2header.textContent = "1. Of the following if statements, which one correctly executes three instructions, if the condition is true?";
-    rotatingPages.appendChild(page2header);
-    page2header.setAttribute("style", "text-align: center;");
+        startTimer();
 
-    var page2div = document.createElement("div");
-    rotatingPages.appendChild(page2div);
-    page2div.setAttribute("style", "margin-left: 200px;");
+        page1.innerHTML = "";
 
-    var page2button1 = document.createElement("button");
-    page2button1.textContent = "A. If (x < 0) a = b * 2; y = x; z = a – y;";
-    page2div.appendChild(page2button1);
+        var page2header = document.createElement("h2");
+        page2header.textContent = "1. Of the following if statements, which one correctly executes three instructions, if the condition is true?";
+        rotatingPages.appendChild(page2header);
+        page2header.setAttribute("style", "text-align: center;");
 
-    var page2button2 = document.createElement("button");
-    page2button2.textContent = "B. { if (x < 0) a = b * 2; y = x; z = a – y; }";
-    page2div.appendChild(page2button2);
+        var page2div = document.createElement("div");
+        rotatingPages.appendChild(page2div);
 
-    var page2button3 = document.createElement("button");
-    page2button3.textContent = "C. If { (x < 0) a = b * 2; y = x; z = a – y ; }";
-    page2div.appendChild(page2button3);
+        var page2button1 = document.createElement("button");
+        page2button1.textContent = "A. If (x < 0) a = b * 2; y = x; z = a – y;";
+        page2div.appendChild(page2button1);
+        page2button1.setAttribute("data-state", "not-answer");
+        page2button1.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-    var page2button4 = document.createElement("button");
-    page2button4.textContent = "D. If (x < 0) { a = b * 2; y = x; z = a – y; }";
-    page2div.appendChild(page2button4);
+        var page2button2 = document.createElement("button");
+        page2button2.textContent = "B. { if (x < 0) a = b * 2; y = x; z = a – y; }";
+        page2div.appendChild(page2button2);
+        page2button2.setAttribute("data-state", "not-answer");
+        page2button2.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-    if (page2button4.onclick) {
+        var page2button3 = document.createElement("button");
+        page2button3.textContent = "C. If { (x < 0) a = b * 2; y = x; z = a – y ; }";
+        page2div.appendChild(page2button3);
+        page2button3.setAttribute("data-state", "not-answer");
+        page2button3.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-        // var page2div::after {
+        var page2button4 = document.createElement("button");
+        page2button4.textContent = "D. If (x < 0) { a = b * 2; y = x; z = a – y; }";
+        page2div.appendChild(page2button4);
+        page2button4.setAttribute("data-state", "answer");
+        page2button4.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-        //     content: 'correct!';
-        // }
+        page2div.addEventListener("click", function(event) {
 
-        score ++;
-        question2();
-        return score;
+            var element = event.target;
 
-    } else {
+            if (element.matches("button")) {
 
-        // var pag2div::after {
+                var answer = element.getAttribute("data-state");
 
-        //     content: 'wrong!';
-        // }
+                if (answer === "answer") {
 
-        secondsLeft - 5;
-        question1();
-        return secondsLeft;
-    }
+                    score ++;
+                    rotatingPages.innerHTML = "";
+                    question2(score, secondsLeft);
 
+                } else if (answer === "not-answer") {
+
+                    secondsLeft - 5;
+                }
+            }
+        }); 
+    });
 }
 
-function question2() {
-
-    page1.setAttribute("style", "visibility: hidden;");
+function question2(score, secondsLeft) {
 
     var page2header = document.createElement("h2");
     page2header.textContent = "2. Which of the sets of statements below will add 1 to x if x is positive and subtract 1 from x if x is negative but leave x alone if x is 0?";
@@ -95,52 +97,55 @@ function question2() {
 
     var page2div = document.createElement("div");
     rotatingPages.appendChild(page2div);
-    page2div.setAttribute("style", "margin-left: 200px;");
 
     var page2button1 = document.createElement("button");
     page2button1.textContent = "A. If (x > 0) x++; else x--;";
     page2div.appendChild(page2button1);
+    page2button1.setAttribute("data-state", "not-answer");
+    page2button1.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button2 = document.createElement("button");
     page2button2.textContent = "B. If (x > 0) x++; else if (x < 0) x--;";
     page2div.appendChild(page2button2);
+    page2button2.setAttribute("data-state", "not-answer");
+    page2button2.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button3 = document.createElement("button");
     page2button3.textContent = "C. If (x == 0) x = 0; else x++; x--;";
     page2div.appendChild(page2button3);
+    page2button3.setAttribute("data-state", "answer");
+    page2button3.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button4 = document.createElement("button");
     page2button4.textContent = "D. X++; x--;";
     page2div.appendChild(page2button4);
+    page2button4.setAttribute("data-state", "not-answer");
+    page2button4.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-    if (page2button3.onclick) {
+    page2div.addEventListener("click", function(event) {
 
-        // var page2div::after {
+        var element = event.target;
 
-        //     content: 'correct!';
-        // }
+        if (element.matches("button")) {
 
-        score ++;
-        question3();
-        return score;
+            var answer = element.getAttribute("data-state");
 
-    } else {
+            if (answer === "answer") {
 
-        // var pag2div::after {
+                score ++;
+                rotatingPages.innerHTML = "";
+                question3(score, secondsLeft);
 
-        //     content: 'wrong!';
-        // }
+            } else {
 
-        secondsLeft - 5;
-        question2();
-        return secondsLeft;
-    }
+                secondsLeft - 5;
+            }
+        }
+    });
 
 }
 
-function question3() {
-
-    page1.setAttribute("style", "visibility: hidden;");
+function question3(score, secondsLeft) {
 
     var page2header = document.createElement("h2");
     page2header.textContent = "3. _______ is the process of finding errors and fixing them within a program.";
@@ -149,52 +154,54 @@ function question3() {
 
     var page2div = document.createElement("div");
     rotatingPages.appendChild(page2div);
-    page2div.setAttribute("style", "margin-left: 200px;");
 
     var page2button1 = document.createElement("button");
     page2button1.textContent = "A. Compiling";
     page2div.appendChild(page2button1);
+    page2button1.setAttribute("data-state", "not-answer");
+    page2button1.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button2 = document.createElement("button");
     page2button2.textContent = "B. Executing";
     page2div.appendChild(page2button2);
+    page2button2.setAttribute("data-state", "not-answer");
+    page2button2.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button3 = document.createElement("button");
     page2button3.textContent = "C. Debugging";
     page2div.appendChild(page2button3);
+    page2button3.setAttribute("data-state", "answer");
+    page2button3.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button4 = document.createElement("button");
     page2button4.textContent = "D. Scanning";
     page2div.appendChild(page2button4);
+    page2button4.setAttribute("data-state", "not-answer");
+    page2button4.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-    if (page2button3.onclick) {
+    page2div.addEventListener("click", function(event) {
 
-        // var page2div::after {
+        var element = event.target;
 
-        //     content: 'correct!';
-        // }
+        if (element.matches("button")) {
 
-        score ++;
-        question4();
-        return score;
+            var answer = element.getAttribute("data-state");
 
-    } else {
+            if (answer === "answer") {
 
-        // var pag2div::after {
+                score ++;
+                rotatingPages.innerHTML = "";
+                question4(score, secondsLeft);
 
-        //     content: 'wrong!';
-        // }
+            } else {
 
-        secondsLeft - 5;
-        question3();
-        return secondsLeft;
-    }
-
+                secondsLeft - 5;
+            }
+        }
+    });
 }
 
-function question4() {
-
-    page1.setAttribute("style", "visibility: hidden;");
+function question4(score, secondsLeft) {
 
     var page2header = document.createElement("h2");
     page2header.textContent = "4. Sal needs to execute a section of code ten times within a program. Compare the selection structures below and select which one meets the needs identified.";
@@ -203,51 +210,54 @@ function question4() {
 
     var page2div = document.createElement("div");
     rotatingPages.appendChild(page2div);
-    page2div.setAttribute("style", "margin-left: 200px;");
 
     var page2button1 = document.createElement("button");
     page2button1.textContent = "A. If-Else";
     page2div.appendChild(page2button1);
+    page2button1.setAttribute("data-state", "not-answer");
+    page2button1.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button2 = document.createElement("button");
     page2button2.textContent = "B. For";
     page2div.appendChild(page2button2);
+    page2button2.setAttribute("data-state", "answer");
+    page2button2.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button3 = document.createElement("button");
     page2button3.textContent = "C. While";
     page2div.appendChild(page2button3);
+    page2button3.setAttribute("data-state", "not-answer");
+    page2button3.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button4 = document.createElement("button");
     page2button4.textContent = "D. If";
     page2div.appendChild(page2button4);
+    page2button4.setAttribute("data-state", "not-answer");
+    page2button4.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-    if (page2button2.onclick) {
+    page2div.addEventListener("click", function(event) {
 
-        // var page2div::after {
+        var element = event.target;
 
-        //     content: 'correct!';
-        // }
+        if (element.matches("button")) {
 
-        score ++;
-        question5();
-        return score;
+            var answer = element.getAttribute("data-state");
 
-    } else {
+            if (answer === "answer") {
 
-        // var pag2div::after {
+                score ++;
+                rotatingPages.innerHTML = "";
+                question5(score, secondsLeft);
 
-        //     content: 'wrong!';
-        // }
+            } else {
 
-        secondsLeft - 5;
-        question4();
-        return secondsLeft;
-    }
+                secondsLeft - 5;
+            }
+        }
+    });
 }    
 
-function question5() {
-
-    page1.setAttribute("style", "visibility: hidden;");
+function question5(score, secondsLeft) {
 
     var page2header = document.createElement("h2");
     page2header.textContent = "5. Which of the following attributes is used to open an hyperlink in new tab?";
@@ -256,75 +266,110 @@ function question5() {
 
     var page2div = document.createElement("div");
     rotatingPages.appendChild(page2div);
-    page2div.setAttribute("style", "margin-left: 200px;");
 
     var page2button1 = document.createElement("button");
     page2button1.textContent = "A. tab";
     page2div.appendChild(page2button1);
+    page2button1.setAttribute("data-state", "not-answer");
+    page2button1.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button2 = document.createElement("button");
     page2button2.textContent = "B. target";
     page2div.appendChild(page2button2);
+    page2button2.setAttribute("data-state", "answer");
+    page2button2.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button3 = document.createElement("button");
     page2button3.textContent = "C. href";
     page2div.appendChild(page2button3);
+    page2button3.setAttribute("data-state", "not-answer");
+    page2button3.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
     var page2button4 = document.createElement("button");
     page2button4.textContent = "D. src";
     page2div.appendChild(page2button4);
+    page2button4.setAttribute("data-state", "not-answer");
+    page2button4.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
 
-    if (page2button2.onclick) {
+    page2div.addEventListener("click", function(event) {
 
-        // var page2div::after {
+        var element = event.target;
 
-        //     content: 'correct!';
-        // }
+        if (element.matches("button")) {
 
-        score ++;
-        thirdPage();
-        return score;
+            var answer = element.getAttribute("data-state");
 
-    } else {
+            if (answer === "answer") {
 
-        // var pag2div::after {
+                score ++;
+                rotatingPages.innerHTML = "";
+                timer.textContent = "";
+                thirdPage(score);
 
-        //     content: 'wrong!';
-        // }
+            } else {
 
-        secondsLeft - 5;
-        question5();
-        return secondsLeft;
-    }
+                secondsLeft - 5;
+            }
+        }
+    });
 }
 
-function thirdPage() {
+function thirdPage(score) {
 
-    page1.setAttribute("style", "visibility: hidden;");
-    
+    clearInterval(timerInterval);
+    rotatingPages.innerHTML = "";
+
     var page3header = document.createElement("h2");
     page3header.textContent = "All done!";
     rotatingPages.appendChild(page3header);
-    page3header.setAttribute("style", "text-align: center;");
+    page3header.setAttribute("style", "text-align: center; margin-bottom: 40px");
 
     var page3paragraph = document.createElement("p");
     page3paragraph.textContent = "Your final score is : " + score;
+    rotatingPages.appendChild(page3paragraph);
 
     var page3input = document.createElement("input");
     rotatingPages.appendChild(page3input)
     page3input.setAttribute("type", "text");
     page3input.setAttribute("placeholder", "Type your initials here to save your score");
+    page3input.setAttribute("style", "margin: 20px auto; display: block;")
+    page3input.setAttribute("size", page3input.getAttribute("placeholder").length)
 
     var page3button = document.createElement("button");
     page3button.textContent = "Submit";
     rotatingPages.appendChild(page3button);
-    page3button.setAttribute("style", "margin: 20px auto; padding: 15px;");
+    page3button.setAttribute("style", "display: block; margin: 20px auto; padding: 15px;");
+    page3button.setAttribute("data-type", "submit");
 
+    rotatingPages.addEventListener("click", function(event) {
+
+        var element = event.target;
+
+        if (element.matches("button")) {
+
+            var answer = element.getAttribute("data-type");
+
+            if (answer === "submit") {
+
+                localStorage.setItem("score", score);
+                localStorage.setItem("initials", page3input.value);
+            }
+        }
+    });
 }
 
-if (startQuiz){
+page4.addEventListener("click", function() {
 
-    startQuiz.addEventListener("click", secondPage());
-}
+    rotatingPages.innerHTML = "";
+
+    var page4header = document.createElement("h2");
+    page4header.textContent = "Your High Score";
+    rotatingPages.appendChild(page4header);
+    page4header.setAttribute("style", "text-align: center;");
+
+    var page4paragraph = document.createElement("p");
+    page4paragraph.textContent = localStorage.getItem ("score", score);
+    rotatingPages.appendChild(page4paragraph);
+});
 
 // question 1-4 complementary of : https://www.proprofs.com/quiz-school/story.php?title=computer-programming-ii-exam-multiple-choice
